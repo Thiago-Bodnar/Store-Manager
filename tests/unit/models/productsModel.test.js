c18dadd220cd0757a10992c49a0ed02eaf03df6a
Thiago <bodnarthiago@gmail.com>
2022-06-29 19:10:29 -0300

    test: testes do productsModel iniciados

---
+++ tests/unit/models/productsModel.test.js
@@ -0,0 +1,23 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const db = require('../../../models/db');
+const productsModel = require('../../../models/productsModel');
+
+describe('productsModel', () => {
+  beforeEach(() => {
+    sinon.restore();
+  });
+
+  describe('/:id',() => { 
+    it('Quando existe, retorna um objeto com as informações corretas', async () => {     
+      sinon.stub(db, 'query').resolves([[{ id: 1, name: 'Martelo de Thor' }]]);
+      const product = await productsModel.get(1);
+      expect(product).to.be.an('object').and.to.have.property('id', 1);
+    });
+    it('Quando não existe, retorna mensagem', async () => {
+      sinon.stub(db, 'query').resolves([[{ message: 'Product not found' }]]);
+      const error = await productsModel.get(69);
+      expect(error).to.be.an('object').and.to.have.property('message', 'Product not found');
+    });
+  });
+});
