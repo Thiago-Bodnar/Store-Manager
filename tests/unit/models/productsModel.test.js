1215a5d05ad90e6c91869c4eeea421443724e36c
Thiago <bodnarthiago@gmail.com>
2022-06-30 17:00:34 -0300

    test: 100% de cobertura em productsModel

--- tests/unit/models/productsModel.test.js
+++ tests/unit/models/productsModel.test.js
@@ -8,7 +8,27 @@ describe('productsModel', () => {
     sinon.restore();
   });
 
-  describe('/:id',() => { 
+  describe('list', () => {
+    it('Lista corretamente os itens', async () => {
+      const list = [
+        { id: 1, name: 'Martelo de Thor' },
+        { id: 2, name: 'Traje de encolhimento' },
+        { id: 3, name: 'Escudo do Capitão América' },
+      ];
+
+      sinon.stub(db, 'query').resolves([[
+        { id: 1, name: 'Martelo de Thor' },
+        { id: 2, name: 'Traje de encolhimento' },
+        { id: 3, name: 'Escudo do Capitão América' }, 
+      ]]);
+
+      const getList = await productsModel.list();
+
+      expect(getList).to.be.deep.eq(list);
+    });
+  });
+
+  describe('get',() => { 
     it('Quando existe, retorna um objeto com as informações corretas', async () => {     
       sinon.stub(db, 'query').resolves([[{ id: 1, name: 'Martelo de Thor' }]]);
 
@@ -16,12 +36,25 @@ describe('productsModel', () => {
 
       expect(product).to.be.an('object').and.to.have.property('id', 1);
     });
+
     it('Quando não existe, retorna mensagem de não encontrado', async () => {
       sinon.stub(db, 'query').resolves([[{ message: 'Product not found' }]]);
 
       const error = await productsModel.get(69);
       
       expect(error).to.be.an('object').and.to.have.property('message', 'Product not found');
+    });
+
+    describe('add', () => {
+      it('Ao receber um valor name, salva o dado no banco e retorna o id', async () => {
+        const expectedId = 1;
+
+        sinon.stub(db, 'query').resolves([{ insertId: expectedId }]);
+
+        const id = await productsModel.add({ name: 'Capa da invisibilidade' });
+
+        expect(id).to.be.eq(expectedId);
+      });
     });
   });
 });
